f3e80bce5d5634d13cc5caefdbeb3bb016d56ccb
revisitor <mikado.brave@gmail.com>
2022-09-26 12:20:18 +0700

    Using ExecutorService to make requests

--- app/src/main/java/ru/mtrefelov/http/MainActivity.kt
+++ app/src/main/java/ru/mtrefelov/http/MainActivity.kt
@@ -2,8 +2,8 @@ package ru.mtrefelov.http
 
 import android.os.Bundle
 import android.widget.Button
-
 import androidx.appcompat.app.AppCompatActivity
+import java.util.concurrent.*
 
 class MainActivity : AppCompatActivity() {
     private val url = "https://api.flickr.com/services/rest/" +
@@ -12,7 +12,9 @@ class MainActivity : AppCompatActivity() {
             "&tags=cat" +
             "&format=json" +
             "&nojsoncallback=1"
-    
+
+    private val httpRequestsExecutor: ExecutorService = Executors.newCachedThreadPool()
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
@@ -20,7 +22,7 @@ class MainActivity : AppCompatActivity() {
         val httpTask = LogHttpResponseRunnable(url)
         val buttonFetchHttp: Button = findViewById(R.id.button_http)
         buttonFetchHttp.setOnClickListener {
-            Thread(httpTask).start()
+            httpRequestsExecutor.execute(httpTask)
         }
 
         val okHttpTask = LogOkHttpResponse(url)
@@ -28,5 +30,10 @@ class MainActivity : AppCompatActivity() {
         buttonFetchOkHttp.setOnClickListener {
             okHttpTask.run()
         }
+    }
+
+    override fun onDestroy() {
+        super.onDestroy()
+        httpRequestsExecutor.shutdown()
     }
 }
